5f892f557052014e7ff1f6ab53ccbd836120a9e7
dcsan <dc@rikai.co>
2020-11-13 22:46:34 -0800

    more messing with animations

--- bureau/marcom/banner01/process.sh
+++ bureau/marcom/banner01/process.sh
@@ -1,29 +1,49 @@
 
 set -x
-dir='./1000'
 
-# offx=(1000-888)/2
-offx=56
-echo $offx
 
-# resize to pow2 for making mp4
+# before using create a folder of 1000
+
+# crop to pow2 for making mp4
+# mp4 has to be powers of 2 aspect ratio
+# based on the files in 1000 width
+# leaves a side padding
+# copes from ./1000 to ./wide
+
+# 4:3
+# 320:420
+# 400:300
+
+# 16:9 -
+# 640:360
+# 960:540
+# 1280:720
+
+
 function resize {
-  FILES=./1000/*jpg
+
+  sourceDir='./960w'
+
+  # crop based on 1000 width
+  imageSize="960:540"
+
+  offsetx=56  # offx=(1000-888)/2
+  echo $offx
+
+  FILES="./${sourceDir}/*jpg"
   for input in $FILES
   do
-
     output="${input/1000/wide}"
     echo "$input -> $output"
-    ffmpeg -y -i $input -vf  "crop=888:500:56:0" $output
-
-    # take action on each file. $f store current file name
-    # cat $f
+    ffmpeg -y -i $input -vf  "crop=${imageSize}" $output
   done
 
 }
 
+# make an mp4 from images
 function makeMov() {
-	cat wide/*.jpg | ffmpeg -y -framerate 2 -f image2pipe -i - banner.mp4
+  inputDir="input"
+	cat ${inputDir}/*.jpg | ffmpeg -y -framerate 2 -f image2pipe -i - banner.mp4
 }
 
 # make a GIF
@@ -35,11 +55,12 @@ function makeMov() {
 function makeGif() {
 	# ffmpeg -i banner.mp4 banner.gif
   # -ss 30 -t 3
-  ffmpeg -y -i banner.mp4 -vf "fps=10,scale=888:-1:flags=lanczos,split[s0][s1];[s0]palettegen[p];[s1][p]paletteuse" -loop 2 banner-888.gif
+  ffmpeg -y -i banner.mp4 -vf "fps=10,scale=960:-1:flags=lanczos,split[s0][s1];[s0]palettegen[p];[s1][p]paletteuse" -loop 2 banner-960.gif
+  ffmpeg -y -i banner.mp4 -vf "fps=10,scale=640:-1:flags=lanczos,split[s0][s1];[s0]palettegen[p];[s1][p]paletteuse" -loop 2 banner-640.gif
   ffmpeg -y -i banner.mp4 -vf "fps=10,scale=480:-1:flags=lanczos,split[s0][s1];[s0]palettegen[p];[s1][p]paletteuse" -loop 2 banner-480.gif
 }
 
-resize
+# resize
 makeMov
 makeGif
 
